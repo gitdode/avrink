cc814d81e4696b60294dc4a1b1123f392624f0f8
Torsten Römer <torsten.roemer@luniks.net>
2023-04-02 05:03:49 +0200

    driving display basically works - draw vertical bars for a test image

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ with an AVR MCU and avr-libc.
 
 Currently implemented:
 
-* Basic SRAM support
-* Incomplete display initialization
+* Basic SRAM support (currently not used as frame buffer)
+* Experimental display initialization
+* Draw a simple "test image"
+
+<img src="https://luniks.net/other/AVRInk-01.jpg" height="405"/>
+
+Thanks to [https://github.com/adafruit/Adafruit_EPD](https://github.com/adafruit/Adafruit_EPD)
+for helping me out!
--- eink.c
+++ eink.c
@@ -50,7 +50,7 @@ static void waitBusy(void) {
 void display(void) {
     PORT_LED |= (1 << PIN_LED);
 
-    printString("----------------\r\n");
+    printString("starting to drive display...\r\n");
 
     // 1. Power On
     // VCI already supplied - could supply by MCU output pin?
@@ -74,56 +74,175 @@ void display(void) {
     dcLow();
     csLow();
     transmit(0x12);
-    dcLow();
-    csHigh();
 
     // - Wait 10ms
     waitBusy();
     _delay_ms(10);
 
-    printString("initial config done\r\n");
-
-    // write the first byte of the user id and read it back
-    // doesn't seem to work - because the driver is not fully initialized yet?
-    dcLow();
-    csLow();
-    transmit(0x38);
-    dcHigh();
-    transmit(123);
-    dcLow();
-    csHigh();
-
-    dcLow();
-    csLow();
-    transmit(0x2e);
-    dcHigh();
-    uint8_t id = transmit(0);
-    dcLow();
-    csHigh();
+    printString("done setting initial configuration\r\n");
     
-    printUint(id);
-
     // 3. Send Initialization Code
     // - Set gate driver output by Command 0x01
+    dcLow();
+    csLow();
+    transmit(0x01);
+    dcHigh();
+    transmit(250 - 1);
+    transmit((250 - 1) >> 8);
+    transmit(0);
+    csHigh();
+    
     // - Set display RAM size by Command 0x11, 0x44, 0x45
+    dcLow();
+    csLow();
+    transmit(0x11);
+    dcHigh();
+    transmit(0x03);
+    csHigh();
+    
+    dcLow();
+    csLow();
+    transmit(0x44);
+    dcHigh();
+    transmit(1);
+    transmit(16);
+    csHigh();
+    
+    dcLow();
+    csLow();
+    transmit(0x45);
+    dcHigh();
+    transmit(0x00);
+    transmit(0x00);
+    transmit(250 - 1);
+    transmit((250 - 1) >> 8);
+    csHigh();
+    
     // - Set panel border by Command 0x3C
+    dcLow();
+    csLow();
+    transmit(0x3c);
+    dcHigh();
+    transmit(0x05);
+    csHigh();
+    
+    printString("done sending initialization code\r\n");
 
     // 4. Load Waveform LUT
     // - Sense temperature by int/ext TS by Command 0x18
     // - Load waveform LUT from OTP by Command 0x22, 0x20 or by MCU
     // - Wait BUSY Low
+    
     waitBusy();
+    
+    /*
+    // Vcom voltage
+    dcLow();
+    csLow();
+    transmit(0x2c);
+    dcHigh();
+    transmit(0x36); // value not in table?
+    csHigh();
+    
+    // gate voltage
+    dcLow();
+    csLow();
+    transmit(0x03);
+    dcHigh();
+    transmit(0x17); // 0x17 = 20V = 0x00 (POR)
+    csHigh();
 
+    // source voltage
+    dcLow();
+    csLow();
+    transmit(0x04);
+    dcHigh();
+    transmit(0x41); (POR)
+    transmit(0x00); // value not in table, should be 0xa8 (POR)?
+    transmit(0x32); (POR)
+    csHigh();
+    */
+    
     // 5. Write Image and Drive Display Panel
     // - Write image data in RAM by Command 0x4E, 0x4F, 0x24, 0x26
+    dcLow();
+    csLow();
+    transmit(0x4e);
+    dcHigh();
+    transmit(1); // offset 1? check 0x44 and 0x45
+    csHigh();
+    
+    dcLow();
+    csLow();
+    transmit(0x4f);
+    dcHigh();
+    transmit(0);
+    transmit(0);
+    csHigh();
+    
+    // write to BW RAM
+    dcLow();
+    csLow();
+    transmit(0x24);
+    dcHigh();
+    // draw vertical bars
+    for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 16; j++) {
+            transmit(0);
+            transmit(0);
+        }
+        for (int j = 0; j < 16; j++) {
+            transmit(255);
+            transmit(255);
+            transmit(255);
+        }
+    }
+    csHigh();
+    
+    printString("done writing data to RAM\r\n");
+    
     // - Set softstart setting by Command 0x0C
+    dcLow();
+    csLow();
+    transmit(0x0c);
+    dcHigh();
+    transmit(0x8b);
+    transmit(0x9c);
+    transmit(0x96);
+    transmit(0x0f);
+    csHigh();
+    
+    printString("done setting softstart\r\n");
+    
     // - Drive display panel by Command 0x22, 0x20
+    dcLow();
+    csLow();
+    transmit(0x22);
+    dcHigh();
+    transmit(0xf4);
+    csHigh();
+    
+    dcLow();
+    csLow();
+    transmit(0x20);
+    csHigh();
+    
     // - Wait BUSY Low
     waitBusy();
 
-    // should do this only when VCI is supplied by MCU pin?
+    printString("done driving display\r\n");
+
     // 6. Power Off
     // - Deep sleep by Command 0x10
+    dcLow();
+    csLow();
+    transmit(0x10);
+    dcHigh();
+    transmit(0x01);
+    csHigh();
+    
+    printString("done setting deep sleep\r\n");
+    
     // - Power OFF
 
     PORT_LED &= ~(1 << PIN_LED);
