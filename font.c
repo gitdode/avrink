1ee52294d5a0008c4e0157ed1ebabd85dc143b24
Torsten Römer <torsten.roemer@luniks.net>
2023-08-31 20:24:23 +0200

    use alternative binary search to get glyphs for displaying strings

--- font.c
+++ font.c
@@ -10,13 +10,26 @@
 #include "font.h"
 
 Glyph getGlyph(Font font, uint16_t code) {
-    for (size_t i = 0; i < font.length; i++) {
-        if (pgm_read_word(&font.glyphs[i].code) == code) {
-            static Glyph glyph;
-            memcpy_P(&glyph, &font.glyphs[i], sizeof (Glyph));
-            
-            return glyph;
+    
+    // https://en.wikipedia.org/wiki/Binary_search_algorithm
+    uint8_t l = 0;
+    uint8_t r = font.length - 1;
+    
+    while (r != l) {
+        uint8_t m = ((l + r) / 2) + 1;
+        if (pgm_read_word(&font.glyphs[m].code) > code) {
+            r = m - 1;
+        } else {
+            l = m;
         }
+    }
+    
+    if (pgm_read_word(&font.glyphs[l].code) == code) {
+        // found code point, get and return glyph
+        static Glyph glyph;
+        memcpy_P(&glyph, &font.glyphs[l], sizeof (Glyph));
+
+        return glyph;
     }
     
     // return question mark if unknown code point
